0a2b03896bd768d33613b364ce713087bdbc7bef
jnfrap <jnfr97@gmail.com>
2022-10-31 17:12:40 +0100

    Ventana de historial funcional

--- src/main/History.java
+++ src/main/History.java
@@ -8,15 +8,28 @@ import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import misc.BDDConnection;
+import misc.Resultados;
+
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 
 public class History extends JFrame {
 
     private JPanel contentPane;
+    private static JTable table;
+    private int pos;
+    private int max;
 
     /**
      * Launch the application.
@@ -38,6 +51,7 @@ public class History extends JFrame {
      * Create the frame.
      */
     public History() {
+        pos=1;
         setUndecorated(true);
         getRootPane().setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.BLACK));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -48,13 +62,39 @@ public class History extends JFrame {
         setContentPane(contentPane);
         contentPane.setLayout(null);
         
+        JLabel lblUserAndDate = new JLabel("..........................................");
+        lblUserAndDate.setBounds(274, 40, 317, 14);
+        contentPane.add(lblUserAndDate);
+        
         JLabel lblhistorial = new JLabel("<html><body><h1>Historial</h1></body></html>");
-        lblhistorial.setBounds(349, 11, 218, 43);
+        lblhistorial.setBounds(346, 0, 218, 43);
         contentPane.add(lblhistorial);
         
         JScrollPane scrollPane = new JScrollPane();
         scrollPane.setBounds(10, 65, 880, 490);
         contentPane.add(scrollPane);
+        
+        max = 0;
+        
+        try {
+            BDDConnection bdd = new BDDConnection();
+            Connection con = bdd.getConnection();
+            
+            PreparedStatement sentence = con.prepareStatement("SELECT max(id) from peticiones");
+            ResultSet rs = sentence.executeQuery();
+            
+            while(rs.next()) {
+                max = rs.getInt(1);
+            }
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        
+        pos=max;
+        
+        //Make a table inside the scrollPane, dont add data yet
+        table = new JTable();
+        scrollPane.setViewportView(table);
         
         JButton btnBack = new JButton("<- Volver");
         btnBack.addActionListener(new ActionListener() {
@@ -72,5 +112,97 @@ public class History extends JFrame {
         });
         btnBack.setBounds(10, 566, 89, 23);
         contentPane.add(btnBack);
+        
+        JButton btnPrevious = new JButton("<");
+        JButton btnNext = new JButton(">");
+        btnNext.setEnabled(false);
+        btnNext.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                btnPrevious.setEnabled(true);
+                pos++;
+                if (pos==max) {
+                    btnNext.setEnabled(false);
+                }
+                
+                String data = fillTable(pos);
+                lblUserAndDate.setText(data);
+            }
+        });
+        btnPrevious.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                btnNext.setEnabled(true);
+                pos--;
+                if (pos==1) {
+                    btnPrevious.setEnabled(false);
+                }
+                
+                String data = fillTable(pos);
+                lblUserAndDate.setText(data);
+            }
+        });
+        btnPrevious.setBounds(223, 36, 41, 23);
+        contentPane.add(btnPrevious);
+        btnNext.setBounds(601, 36, 41, 23);
+        contentPane.add(btnNext);
+        
+        lblUserAndDate.setText(fillTable(pos));
+    }
+    
+    private static String fillTable(int pos){
+        
+        //Create random data to add to the table
+        String[] columnNames = {"Nombre", "Idema", "Temperatura", "Humedad", "Viento", "Fecha", "Suelo"};
+
+        //Create a model for the table
+        DefaultTableModel model = new DefaultTableModel();
+        model.setColumnIdentifiers(columnNames);
+        
+        int userID = 0;
+        String date = null;
+        String email = null;
+        try {
+            BDDConnection bdd = new BDDConnection();
+            Connection con = bdd.getConnection();
+            
+            PreparedStatement sentence = con.prepareStatement("SELECT user_id,date FROM peticiones WHERE id=?");
+            sentence.setInt(1, pos);
+            ResultSet rs = sentence.executeQuery();
+            
+            while(rs.next()) {
+                userID = rs.getInt(1);
+                date = rs.getString(2);
+            }
+            
+            PreparedStatement sentence2 = con.prepareStatement("SELECT email from usuarios where id=?");
+            sentence2.setInt(1, userID);
+            ResultSet rs2 = sentence2.executeQuery();
+            
+            while(rs2.next()) {
+                email = rs2.getString(1);
+            }
+            
+            PreparedStatement sentence3 = con.prepareStatement("SELECT * from llamadas where peticion_id=?");
+            sentence3.setInt(1, pos);
+            ResultSet rs3 = sentence3.executeQuery();
+            
+            while(rs3.next()) {
+                String nombre = rs3.getString(3);
+                String idema = rs3.getString(4);
+                String temperatura = rs3.getString(5);
+                String humedad = rs3.getString(6);
+                String velViento = rs3.getString(7);
+                String fecha = rs3.getString(8);
+                String suelo = rs3.getString(9);
+                
+                model.addRow(new Object[]{nombre+"",idema+"",temperatura+"",humedad+"",velViento+"",fecha+"",suelo+""});
+            }
+            
+            bdd.closeConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        table.setModel(model);
+        
+        return email+", "+date;
     }
 }
